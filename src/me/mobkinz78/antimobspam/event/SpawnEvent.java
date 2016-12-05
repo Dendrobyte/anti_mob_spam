2f9cba28af8c35acbb60ff61589957f0c0466e15
cat <cat@catcake.xyz>
2016-12-04 20:59:47 -0500

    Pure refactor. WAY cleaner

--- src/me/mobkinz78/antimobspam/event/SpawnEvent.java
+++ src/me/mobkinz78/antimobspam/event/SpawnEvent.java
@@ -1,6 +1,5 @@
 package me.mobkinz78.antimobspam.event;
 
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.CreatureSpawnEvent;
@@ -10,40 +9,21 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
  */
 public class SpawnEvent implements Listener {
 
+     private static final CreatureSpawnEvent.SpawnReason SPAWNER = CreatureSpawnEvent.SpawnReason.SPAWNER;
+     private static final CreatureSpawnEvent.SpawnReason SPAWNER_EGG = CreatureSpawnEvent.SpawnReason.SPAWNER_EGG;
+     private static final CreatureSpawnEvent.SpawnReason DISPENSE_EGG = CreatureSpawnEvent.SpawnReason.DISPENSE_EGG;
+     private static final CreatureSpawnEvent.SpawnReason EGG = CreatureSpawnEvent.SpawnReason.EGG;
+
     @EventHandler
     public void creatureSpawn(CreatureSpawnEvent event){
-        Entity entity = event.getEntity();
-        //Spawn Reasons
-        CreatureSpawnEvent.SpawnReason dispenser = CreatureSpawnEvent.SpawnReason.DISPENSE_EGG;
-        CreatureSpawnEvent.SpawnReason egg = CreatureSpawnEvent.SpawnReason.EGG;
-        CreatureSpawnEvent.SpawnReason spawner_egg = CreatureSpawnEvent.SpawnReason.SPAWNER_EGG;
-        CreatureSpawnEvent.SpawnReason spawner = CreatureSpawnEvent.SpawnReason.SPAWNER;
+        CreatureSpawnEvent.SpawnReason spawnReason = event.getSpawnReason();
 
-        //Dispensers, DISPENSE_EGG
-        if(event.getSpawnReason() == dispenser){
-            System.out.println("A mob has been spawned by DISPENSE_EGG");
-            //If a dispenser can not be found...
-            //Use location to register if there are more than X mobs in an area Y large.
-            //If there are any players nearby, send them a message.
-        }
-        //Chicken Egg, EGG
-        if(event.getSpawnReason() == egg){
-            System.out.println("A mob has been spawned by EGG");
-        }
-        //Spawner Egg, SPAWNER_EGG
-        if(event.getSpawnReason() == spawner_egg){
-            System.out.println("A mob has been spawned by SPAWNER_EGG");
-        }
-        //Spawner, SPAWNER (in case of hackers)
-        if(event.getSpawnReason() == spawner){
-            System.out.println("A mob has been spawned by SPAWNER");
-        }
-        //Chunk Gen (possible future update for max mobs in a chunk)
-        /*
-        Get the spawned entity, get the chunk it's in.
-        If that chunk already has X amount of entities of the same type, cancel spawn event.
-        Make specific per world.
-         */
+        // I don't know why the spawner check is actually necissary but w/e
+        if (!spawnReason.name().contains("EGG") && spawnReason != CreatureSpawnEvent.SpawnReason.SPAWNER)
+            // We aren't checking these so...
+            return;
+        System.out.printf("A mob has been spawned via %s", spawnReason.name());
+
     }
 
 }
